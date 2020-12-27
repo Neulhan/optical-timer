d3ce454dcf6c1e01c93b6f4d3b73f1ab5ed5f88a
neulhan <neulhan@lawandgood.com>
2020-12-27 18:06:37 +0900

    config package

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const createWindow = () => {
     webPreferences: {
       nodeIntegration: true,
     },
-    icon: path.join(__dirname, "assets/icons/png/64x64.png"),
+    icon: path.join(__dirname, "./assets/icons/png/64x64.png"),
   });
 
   // and load the index.html of the app.
